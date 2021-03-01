9592f9e876bf7e31af017366a1bdc3972ffaaad9
Matheus-Lenzi <matheus_lenzi10@hotmail.com>
2021-03-01 01:37:55 -0300

    Quinto aula - implementação do modal do LevelUp (Deploy na Vercel)

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -13,60 +13,54 @@ export default function Countdown() {
         resetCountdown
     } = useContext(CountdownContext)
 
-    
+
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
 
     return (
-        <ChallengesProvider>
-            <CountdownProvider>
+        <div>
+            <div className={styles.countdownContainer}>
                 <div>
-                    <div className={styles.countdownContainer}>
-                        <div>
-                            <span>{minuteLeft}</span>
-                            <span>{minuteRight}</span>
-                        </div>
-                        <span>:</span>
-                        <div>
-                            <span>{secondLeft}</span>
-                            <span>{secondRight}</span>
-                        </div>
-                    </div>
-
-                    {hasFinished ? (
-                        <button
-                            disabled
-                            className={styles.countdownButton}
-                        >
-                            Ciclo encerrado
-                        </button>
-
-                    ) : (
-                            <>
-                                {isActive ? (
-                                    <button
-                                        type="button"
-                                        className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
-                                        onClick={resetCountdown}
-                                    >
-                                        Abandonar ciclo
-                                    </button>
-                                ) : (
-                                        <button
-                                            type="button"
-                                            className={styles.countdownButton}
-                                            onClick={startCountdown}
-                                        >
-                                            Iniciar um ciclo
-                                        </button>
-                                    )}
-                            </>
-                        )}
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
                 </div>
-            </CountdownProvider>
-        </ChallengesProvider>
+                <span>:</span>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
+            </div>
 
+            {hasFinished ? (
+                <button
+                    disabled
+                    className={styles.countdownButton}
+                >
+                    Ciclo encerrado
+                </button>
 
+            ) : (
+                    <>
+                        {isActive ? (
+                            <button
+                                type="button"
+                                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                                onClick={resetCountdown}
+                            >
+                                Abandonar ciclo
+                            </button>
+                        ) : (
+                                <button
+                                    type="button"
+                                    className={styles.countdownButton}
+                                    onClick={startCountdown}
+                                >
+                                    Iniciar um ciclo
+                                </button>
+                            )}
+                    </>
+                )}
+        </div>
     );
 }
